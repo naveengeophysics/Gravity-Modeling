3877f36d9612586e124b98faee3dfe7e7cec221d
naveengeophysics <79416402+naveengeophysics@users.noreply.github.com>
2022-03-09 23:04:02 +0530

    Update sphere_anomaly.m

--- sphere_anomaly.m
+++ sphere_anomaly.m
@@ -3,9 +3,9 @@ function g = sphere_anomaly(R,rho,z)
     K = 4*pi*G/3;
     [x,y] = meshgrid(-1000:10:1000,-1000:10:1000);
     g = (K*R*R*R*rho*z./(x.*x + y.*y + z.*z).^1.5)*10e+5;
-%     surf(x,y,g);
-%     xlabel('X (mtr.) ----->');
-%     ylabel('Y (mtr.)------>');
-%     zlabel('\Delta g (mGal) ---->');
-%     title('Gravity Anomaly due to a Sphere');
+    surf(x,y,g);
+    xlabel('X (mtr.) ----->');
+    ylabel('Y (mtr.)------>');
+    zlabel('\Delta g (mGal) ---->');
+    title('Gravity Anomaly due to a Sphere');
 end
